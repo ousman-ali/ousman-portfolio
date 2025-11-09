cb175debb81a1c2090ec3be76b41c569f5d92d71
ousman-ali <oasgerbalhl@gmail.com>
2025-11-09 08:28:47 +0300

    new update

--- src/data.js
+++ src/data.js
@@ -1,6 +1,38 @@
 export const DoneProjects = [
   {
     id: 1,
+    title: "StarFamily Industry PLC",
+    img: "projects/starfamily.jpg",
+    desc: "The starFamily Industry PLC Website, developed using the Laravel - backend, Next.js - dashboard and React - ui. serves as a professional showcase for starFamily to display their works, services, projects, and products. With a visually appealing layout, users can effortlessly navigate through various sections, including project galleries, case studies, and a company bio. The site features interactive elements like image sliders and hover effects, enhancing user engagement. Additionally, it allows for easy updates and management of content, ensuring that the website remains current and reflective of the company's evolving expertise and style.",
+    live: "https://starfamilyindustryplc.com/",
+    technologies: ["React.js", "Next.js", "Laravel", "Bootstrap", "css"],
+    githubLink:
+      "https://github.com/keradiontechnology/StarFamily_React_Web_Frontend",
+    linkedlnLink: "",
+  },
+  {
+    id: 2,
+    title: "Sheger Laundry Management System",
+    img: "projects/laundry.jpg",
+    desc: "The Sheger Laundry Management System, built with Laravel and Blade, is a comprehensive platform designed to streamline laundry operations. It includes powerful modules for order tracking, customer management, stock and inventory control, and employee management. The intuitive dashboard provides real-time insights into orders, services, and stock levels, enabling efficient workflow management and smooth day-to-day operations.",
+    live: "https://shegerautomaticlaundryservice.com/login",
+    technologies: ["Laravel", "Bootstrap"],
+    githubLink:
+      "https://github.com/keradiontechnology/sheger-laundry-management-system",
+    linkedlnLink: "",
+  },
+  {
+    id: 3,
+    title: "Dematsa Trading PLC",
+    img: "projects/dematsa.jpg",
+    desc: "The Dematsa Trading website, developed using Laravel for the backend, Next.js for the dashboard and user interface, serves as a professional platform to showcase the company’s products, services, and projects. With a modern and visually engaging design, users can easily navigate through product catalogs, service listings, and company information. The site includes interactive elements such as image sliders and hover effects to enhance engagement, while its dynamic content management ensures smooth updates and a consistently up-to-date presentation of the company’s offerings.",
+    live: "https://dematsatradingplc.com/",
+    technologies: ["Next.js", "Laravel", "Bootstrap", "css"],
+    githubLink: "https://github.com/keradiontechnology/dematsa-trading-website",
+    linkedlnLink: "",
+  },
+  {
+    id: 4,
     title: "Social Media App",
     img: "projects/social.png",
     desc: "The Social Media App, developed using the MERN stack, offers users a dynamic platform to connect, share, and engage with friends and communities. With a clean and intuitive interface, users can create profiles, post updates, share images, and comment on each other's content. The app features real-time notifications, private messaging, and customizable friend lists, fostering meaningful interactions. Additionally, advanced search functionality and user-generated content categorization enhance the overall experience, making it easy for users to discover new connections and engage with relevant topics.",
@@ -17,7 +49,7 @@ export const DoneProjects = [
     linkedlnLink: "",
   },
   {
-    id: 2,
+    id: 5,
     title: "Booking App",
     img: "projects/book.png",
     desc: "The Bus Station Booking Web App is a user-friendly platform designed to streamline the process of booking bus tickets for travelers. Built using the MERN stack, it features a simple interface where users can easily search for available routes, view schedules, and compare prices from various bus operators. The app provides real-time updates on bus arrivals and departures, along with features like user reviews, seat selection, and the ability to manage bookings, ensuring a seamless travel experience from start to finish.",
@@ -28,7 +60,7 @@ export const DoneProjects = [
       "https://www.linkedin.com/posts/ousman-ali-8271182b3_webdevelopment-fullstack-bookingwebapp-activity-7222908141498216448-SZMG?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEtNR58BPM0rM47K-SWp174MlplkiysyeXc",
   },
   {
-    id: 3,
+    id: 6,
     title: "E-commerce Website",
     img: "projects/ecom.png",
     desc: "The E-commerce Website, built with the MERN stack, provides a seamless shopping experience for users looking to buy a wide range of products online. Featuring an intuitive interface, customers can easily browse categories, filter products, and read detailed descriptions and reviews. The site supports user registration and secure authentication, allowing for personalized shopping experiences and wish lists. With a responsive design, real-time inventory updates, and efficient search functionality, the website ensures that users can find and purchase their desired items effortlessly, all while enjoying smooth navigation and quick load times.",
@@ -43,48 +75,6 @@ export const DoneProjects = [
     githubLink: "https://github.com/ousman-ali/Ecommerce_Web_App",
     linkedlnLink:
       "https://www.linkedin.com/posts/ousman-ali-8271182b3_project-title-mern-stack-e-commerce-web-activity-7338444140763901953-BZgm?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEtNR58BPM0rM47K-SWp174MlplkiysyeXc",
-  },
-  {
-    id: 4,
-    title: "Portfolio Website",
-    img: "projects/portfolio.png",
-    desc: "The Portfolio Website, developed using the MERN stack, serves as a professional showcase for individuals to display their skills, projects, and accomplishments. With a visually appealing layout, users can effortlessly navigate through various sections, including project galleries, case studies, and a personal bio. The site features interactive elements like image sliders and hover effects, enhancing user engagement. Additionally, it allows for easy updates and management of content, ensuring that the portfolio remains current and reflective of the creator's evolving expertise and style. Overall, it provides a compelling platform for individuals to present their work to potential employers and clients.",
-    live: "",
-    technologies: ["React.js", "sass", "node.js", "express.js", "mongoDB"],
-    githubLink: "https://github.com/ousman-ali/OAS-Professional-Portfolio",
-    linkedlnLink: "",
-  },
-  {
-    id: 5,
-    title: "StarFamily Industry PLC",
-    img: "projects/starfamily.jpg",
-    desc: "The starFamily Industry PLC Website, developed using the Laravel - backend, Next.js - dashboard and React - ui. serves as a professional showcase for starFamily to display their works, services, projects, and products. With a visually appealing layout, users can effortlessly navigate through various sections, including project galleries, case studies, and a company bio. The site features interactive elements like image sliders and hover effects, enhancing user engagement. Additionally, it allows for easy updates and management of content, ensuring that the website remains current and reflective of the company's evolving expertise and style.",
-    live: "https://starfamilyindustryplc.com/",
-    technologies: ["React.js", "Next.js", "Laravel", "Bootstrap", "css"],
-    githubLink:
-      "https://github.com/keradiontechnology/StarFamily_React_Web_Frontend",
-    linkedlnLink: "",
-  },
-  {
-    id: 5,
-    title: "Sheger Laundry Management System",
-    img: "projects/laundry.jpg",
-    desc: "The Sheger Laundry Management System, built with Laravel and Blade, is a comprehensive platform designed to streamline laundry operations. It includes powerful modules for order tracking, customer management, stock and inventory control, and employee management. The intuitive dashboard provides real-time insights into orders, services, and stock levels, enabling efficient workflow management and smooth day-to-day operations.",
-    live: "https://shegerautomaticlaundryservice.com/login",
-    technologies: ["Laravel", "Bootstrap"],
-    githubLink:
-      "https://github.com/keradiontechnology/sheger-laundry-management-system",
-    linkedlnLink: "",
-  },
-  {
-    id: 5,
-    title: "Dematsa Trading PLC",
-    img: "projects/dematsa.jpg",
-    desc: "The Dematsa Trading website, developed using Laravel for the backend, Next.js for the dashboard and user interface, serves as a professional platform to showcase the company’s products, services, and projects. With a modern and visually engaging design, users can easily navigate through product catalogs, service listings, and company information. The site includes interactive elements such as image sliders and hover effects to enhance engagement, while its dynamic content management ensures smooth updates and a consistently up-to-date presentation of the company’s offerings.",
-    live: "https://dematsatradingplc.com/",
-    technologies: ["Next.js", "Laravel", "Bootstrap", "css"],
-    githubLink: "https://github.com/keradiontechnology/dematsa-trading-website",
-    linkedlnLink: "",
   },
 ];
 
