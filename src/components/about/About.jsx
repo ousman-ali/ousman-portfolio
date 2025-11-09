6c27584c2d958f42f905d42e6f6c98ec2d23dc07
ousman-ali <oasgerbalhl@gmail.com>
2025-11-09 08:12:52 +0300

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './about.scss';
+import React from "react";
+import "./about.scss";
 
 const About = () => {
   return (
@@ -7,18 +7,14 @@ const About = () => {
       <div className="about-content">
         <h2>About Me</h2>
         <p>
-          I'm <strong>Ousman Ali</strong>, an Electrical Engineering graduate and self-taught software developer with a strong passion
-          for building digital solutions. I specialize in developing web applications using the <strong>MERN stack</strong> and 
-          creating mobile apps with <strong>Flutter</strong>. For over a year, I've been consistently working on personal projects 
-          to enhance my skills and deepen my understanding. I'm a dedicated, detail-oriented developer who’s always eager to learn 
-          and grow in the tech space.
-        </p>
-
-        <p>
-          I enjoy working on both <strong>frontend</strong> and <strong>backend</strong> development,
-           and I'm always exploring new technologies and tools to improve the user experience and development process.
-          Beyond coding, I enjoy learning, sharing knowledge, 
-          and collaborating with others to bring ideas to life.
+          I'm <strong>Ousman Ali</strong>, a self-taught full-stack software
+          developer with a strong passion for building scalable and user-focused
+          digital solutions. I specialize in developing modern web applications
+          using <strong>Different Frame Works</strong>. For over a year, I’ve
+          been consistently working on personal and professional projects to
+          sharpen my skills and broaden my expertise. I’m a dedicated,
+          detail-oriented developer who’s always eager to learn, adapt, and
+          contribute to meaningful software solutions.
         </p>
       </div>
     </section>
